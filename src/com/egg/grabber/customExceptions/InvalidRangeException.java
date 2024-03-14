9d94e34ed84b3479044c6ed1d38502fabd6b8bfb
Egg-03 <111327101+Egg-03@users.noreply.github.com>
2024-03-14 23:04:14 +0530

    Removed InvalidRangeException
    It now gets shown in the ExceptionUI dialog box instead

--- src/com/egg/grabber/customExceptions/InvalidRangeException.java
+++
@@ -1,9 +0,0 @@
-package com.egg.grabber.customExceptions;
-
-public class InvalidRangeException extends Exception {
-	private static final long serialVersionUID = 5130115225773334151L;
-	
-	public InvalidRangeException(String errorMessage) {
-		super(errorMessage);
-	}
-}
--- src/com/egg/grabber/main/WebDriverLauncher.java
+++ src/com/egg/grabber/main/WebDriverLauncher.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-import com.egg.grabber.customExceptions.InvalidRangeException;
+import com.egg.grabber.customExceptions.ExceptionUI;
 
 public class WebDriverLauncher {
 	private WebDriver driver;
@@ -24,24 +24,27 @@ public class WebDriverLauncher {
 		driver = new ChromeDriver(coption);
 	}
 	
-	public void dateWiseSales(String year, String month, String start, String end) throws InvalidRangeException {
+	public void dateWiseSales(String year, String month, String start, String end){
 		if(Integer.parseInt(start)>Integer.parseInt(end)) {
-			throw new InvalidRangeException("Begin Range has to be smaller than the End Range");
-			}
-		
-		driver.navigate().to("https://remedy.onlinehealthcaremanagement.com/mis/DateWiseSalesReport/0/0/0");
-		for(int i=Integer.parseInt(start) ; i<=Integer.parseInt(end) ; i++) {
-			//fill start date
-			driver.findElement(By.id("StartDate")).sendKeys(i+"/"+month+"/"+year);
-			//fill end date
-			driver.findElement(By.id("EndDate")).sendKeys(i+"/"+month+"/"+year);
-			
-			//export-to-excel-button
-			driver.findElement(By.className("btn-info")).click();
-			
-			//clear the text fields
-			driver.findElement(By.id("StartDate")).clear();
-			driver.findElement(By.id("EndDate")).clear();
+			ExceptionUI ecp = new ExceptionUI("Begin Range cannot be greater than End Range. Please close this message and    try again.");
+			ecp.setVisible(true);
+			driver.quit();
+		}
+		else {
+			driver.navigate().to("https://remedy.onlinehealthcaremanagement.com/mis/DateWiseSalesReport/0/0/0");
+			for(int i=Integer.parseInt(start) ; i<=Integer.parseInt(end) ; i++) {
+				//fill start date
+				driver.findElement(By.id("StartDate")).sendKeys(i+"/"+month+"/"+year);
+				//fill end date
+				driver.findElement(By.id("EndDate")).sendKeys(i+"/"+month+"/"+year);
+				
+				//export-to-excel-button
+				driver.findElement(By.className("btn-info")).click();
+				
+				//clear the text fields
+				driver.findElement(By.id("StartDate")).clear();
+				driver.findElement(By.id("EndDate")).clear();
+		    }
 		}
 	}
 }
